b6e4569d623876938a1c63a709b4f4bb369fabe4
BRUNNO FERNANDES LEWIN AUGUSTO <brunno.lewin@dominio.ufms.br>
2023-10-19 12:31:41 -0400

    US1: Adicionado model, controller e migration de inscrição

---
+++ database/migrations/2023_10_19_123505_create_inscricaos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInscricaosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inscricaos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('atividade_id');
+            $table->string('nome');
+            $table->string('cpf');
+            $table->string('email')->unique();
+            $table->boolean('checkin');
+            $table->timestamps();
+
+            $table->foreign('atividade_id')->references('id')->on('atividade');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inscricaos');
+    }
+}
